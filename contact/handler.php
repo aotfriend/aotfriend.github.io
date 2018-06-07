c54c18721ecfc4aa927ae398c938012b0f974bba
aotdragonsfamily <38174599+aotdragonsfamily@users.noreply.github.com>
2018-06-07 20:14:41 +0700

    Add files via upload

--- contact/handler.php
+++ contact/handler.php
@@ -1,13 +1,13 @@
 <?php 
-if(isset($_POST['submit'])){
-    $to = "friendrpgth@gmail.com"; // this is your Email address
+//if(isset($_POST['submit'])){
+    $to = "friendrpgth@gmail.com";
     $name = $_POST['name'];
-    $subject = "Contact from AOTTG Friend Mod";
+    $subject = "AOTTG Friend Mod";
     $message = $name . " wrote the following:" . "\n\n" . $_POST['comment'];
     $headers = "[" . $_POST['section'] . "] From:" . $name;
     mail($to,$subject,$message,$headers);
     echo "Mail Sent. Thank you " . $name . ", we will accept your submit shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
-    }
+    //}
 ?>
