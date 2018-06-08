27671b2725b3ead8a8da852a2d8412c04ea58682
aotdragonsfamily <38174599+aotdragonsfamily@users.noreply.github.com>
2018-06-09 04:55:34 +0700

    Add files via upload

--- js/ads.js
+++ js/ads.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   var nDIV = document.createElement("DIV");
   nDIV.className = "adsblock-content";
-  nDIV.innerHTML = "<ins class=\"adsbygoogle\" style=\"display:inline-block;width:100%;\" data-ad-client=\"ca-pub-6915385022840539\" data-ad-slot=\"2259532232\"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>";
+  nDIV.innerHTML = "<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script><ins class=\"adsbygoogle\" style=\"display:inline-block;width:100%;\" data-ad-client=\"ca-pub-6915385022840539\" data-ad-slot=\"2259532232\"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>";
   var DIV = document.createElement("DIV");
   DIV.className = "adsblock";
   DIV.title = "Google Ads";
